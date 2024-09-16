fe6ebd4932310db4dd7dcab2478b5be099e3e095
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-09-16 18:46:38 +0900

    define images list

---
+++ app/01/lib.ts
@@ -0,0 +1,64 @@
+import { ImageProps } from "./types";
+
+export const images: ImageProps[] = [
+  {
+    title: "Fixing computer",
+    src: "https://images.unsplash.com/photo-1721332149274-586f2604884d",
+    origin: "https://unsplash.com/photos/nplkFSNschY",
+  },
+  {
+    title: "Colorful smoke",
+    src: "https://images.unsplash.com/photo-1558470598-a5dda9640f68",
+    origin: "https://unsplash.com/photos/husQ4tkR8Z8",
+  },
+  {
+    title: "Colorful roses",
+    src: "https://images.unsplash.com/photo-1433888104365-77d8043c9615",
+    origin: "https://unsplash.com/photos/pXmbsF70ulM",
+  },
+  {
+    title: "Rainbow circles",
+    src: "https://images.unsplash.com/photo-1490049350474-498de43bc885",
+    origin: "https://unsplash.com/photos/3shfnfzdFVc",
+  },
+  {
+    title: "Rainbow flag with twinkling background",
+    src: "https://images.unsplash.com/photo-1608048608332-1cda2071e04d",
+    origin: "https://unsplash.com/photos/sAMmA7cUnWk",
+  },
+  {
+    title: "Water near mountain during blue hour",
+    src: "https://images.unsplash.com/flagged/photo-1554443883-83db889b4007",
+    origin: "https://unsplash.com/photos/D1G2q4",
+  },
+  {
+    title: "Brown dog with red scarf",
+    src: "https://images.unsplash.com/photo-1523592234462-547a13c237d8",
+    origin: "https://unsplash.com/photos/LbOJI4yukSQ",
+  },
+  {
+    title: "Bunch of code",
+    src: "https://images.unsplash.com/photo-1515879218367-8466d910aaa4",
+    origin: "https://unsplash.com/photos/ieic5Tq8YMk",
+  },
+  {
+    title: "German shepherd puppies",
+    src: "https://images.unsplash.com/photo-1519150268069-c094cfc0b3c8",
+    origin: "https://unsplash.com/photos/ECjHeJtRznQ",
+  },
+  {
+    title: "Kitties in a picnic basket",
+    src: "https://images.unsplash.com/photo-1517331156700-3c241d2b4d83",
+    origin: "https://unsplash.com/photos/YCPkW_r_6uA",
+  },
+  {
+    title: "Brown cat lying over black cat",
+    src: "https://images.unsplash.com/photo-1532386236358-a33d8a9434e3",
+    origin: "https://unsplash.com/photos/1l2waV8glIQ",
+  },
+  {
+    title: "Purple sky with pink clouds over mountain and lake",
+    src: "https://images.unsplash.com/photo-1692212327613-cdfb5a41e3e6",
+    origin: "https://unsplash.com/photos/KI_z680ZbSw",
+  },
+];
