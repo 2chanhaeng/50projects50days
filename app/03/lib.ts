a5cfc460ebf3f87c5094125f2f6dbb03836bbe32
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-09-17 04:21:48 +0900

    define menuItems

---
+++ app/03/lib.ts
@@ -0,0 +1,14 @@
+export const menuItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "https://github.com/2chanhaeng#2chanhaeng",
+  },
+  {
+    title: "Contact",
+    href: "https://github.com/2chanhaeng",
+  },
+];
