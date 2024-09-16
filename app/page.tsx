874ea9705a03120d95ab1acc7764a1384ca2c256
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-09-16 18:45:55 +0900

    show children page links in root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
+import path from "path";
+import Link from "next/link";
+import { getChildrenPages } from "./lib";
+
 export default function Home() {
+  const rootDir = path.join(process.cwd(), "app");
+  const pages = getChildrenPages(rootDir);
+
   return (
     <main>
+      <ul>
+        {pages.map((subPage) => (
+          <li key={subPage}>
+            <Link href={`/${subPage}`}>{subPage}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
