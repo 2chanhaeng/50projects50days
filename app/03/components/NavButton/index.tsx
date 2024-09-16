487b798dd22cc2db905e8a8cf7342fd879ef91cb
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-09-17 04:21:16 +0900

    define NavButton component

---
+++ app/03/components/NavButton/index.tsx
@@ -0,0 +1,7 @@
+export default function NavButton() {
+  return (
+    <label className="absolute -top-24 -left-24 size-48 rounded-full bg-red-400">
+      <input type="checkbox" hidden />
+    </label>
+  );
+}
