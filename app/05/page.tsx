12fba811a257bd8372616cec96b03c2c0962497c
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-09-18 18:02:39 +0900

    define Day05 page

---
+++ app/05/page.tsx
@@ -0,0 +1,11 @@
+import Background from "./components/Background";
+import FilterCounter from "./components/FilterCounter";
+
+export default async function Day05() {
+  return (
+    <main>
+      <Background />
+      <FilterCounter />
+    </main>
+  );
+}
