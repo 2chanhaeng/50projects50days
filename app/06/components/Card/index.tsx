2c2453081d4754b7d989f7e7199b5062fe8bad21
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-09-18 19:57:47 +0900

    define Card component

---
+++ app/06/components/Card/index.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+import { useInView } from "./hooks";
+
+export default function Card({ isLeft }: { isLeft: boolean }) {
+  const { inView, ref } = useInView();
+  const margin = isLeft ? "margin-left" : "margin-right";
+  const style = { [margin]: "200vw" };
+
+  useEffect(
+    () => ref.current?.style.setProperty(margin, inView ? "0" : "200vw"),
+    [inView, margin, ref]
+  );
+
+  return (
+    <div
+      className="rounded-lg bg-blue-400 text-white w-1/2 h-[25vh] text-center content-center ease-in-out duration-300"
+      ref={ref}
+      style={style}
+    >
+      Content
+    </div>
+  );
+}
