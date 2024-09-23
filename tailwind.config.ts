458ec0eeefdfee734009a3a7e740329b54bebfc5
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-09-23 14:09:23 +0900

    append wave transition function

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      transitionTimingFunction: {
+        wave: "cubic-bezier(0.68, -0.55, 0.265, 1.55)",
+      },
     },
   },
   plugins: [],
